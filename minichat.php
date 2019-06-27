c285621b749a9244e2bd351599959ac628d9f5d5
metabis <metabis.n83gomes@gmail.com>
2019-06-27 20:10:30 +0200

    html/css

--- minichat.php
+++ minichat.php
@@ -3,15 +3,23 @@
     <head>
         <meta charset="utf-8" />
         <title>Mini-chat</title>
+        <link rel="stylesheet" href="design.css" type="text/css" title="Design" />
     </head>
-    <style>
-    form
-    {
-        text-align:center;
-    }
-    </style>
-    <body>
 
+    <body>
+<div id="global">
+  <table border = 0 >
+    <tr>
+  <td colspan=0><div="entete" ></td>
+    </tr>
+   <tr>
+   <td height=20px> &nbsp;  </td>
+   <td >&nbsp;</td>
+
+   </tr>
+   <tr>
+
+   <td><div id="corps">
     <form action="minichat_post.php" method="post">
         <p>
         <label for="pseudo">Pseudo</label> : <input type="text" name="pseudo" id="pseudo" /><br />
@@ -20,7 +28,18 @@
         <input type="submit" value="Envoyer" />
 	</p>
     </form>
+  </div></td>
+   <td >&nbsp;</td>
+  <td valign=top>
+  </div></td>
+   </tr>
+   <tr>
+   <td heigh=20px> &nbsp;  </td>
+   <td >&nbsp;</td>
 
+   </tr>
+   <tr>
+   <td colspan=3>
 <?php
 // Connexion à la base de données
 try
@@ -44,5 +63,9 @@ while ($donnees = $reponse->fetch())
 $reponse->closeCursor();
 
 ?>
-    </body>
+</td>
+ </tr>
+ </table>
+ </div>
+ </body>
 </html>
