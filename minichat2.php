ef0162118dec393c38e214663c582bba6ac589a3
metabis <metabis.n83gomes@gmail.com>
2019-06-27 18:17:46 +0200

    test

--- minichat2.php
+++ minichat2.php
@@ -12,6 +12,14 @@
     </style>
     <body>
 
+    <form action="minichat_post.php" method="post">
+        <p>
+        <label for="pseudo">Pseudo</label> : <input type="text" name="pseudo" id="pseudo" /><br />
+        <label for="message">Message</label> :  <input type="text" name="message" id="message" /><br />
+
+        <input type="submit" value="Envoyer" />
+	</p>
+    </form>
 
 <?php
 if (isset($_POST['pseudo']) and isset($_POST['message']) //Si les messages existent
@@ -28,33 +36,6 @@ if (isset($_POST['pseudo']) and isset($_POST['message']) //Si les messages exist
     mysql_close();
     }
 }
-?>
-
-<form action="minichat.php?message=envoyer" method="post">
-Votre pseudo : <input type="text" name="pseudo" /><br />
-Votre message : <input type="text" name="message" /><br />
-<input type="submit" value="Envoyer" />
-</form>
-
-<?php
-// Maintenant on doit récupérer les 10 dernières entrées de la table
-// On se connecte d'abord à MySQL :
-mysql_connect("localhost", "root", "");
-mysql_select_db("minichat");
-// On utilise la requête suivante pour récupérer les 10 derniers messages :
-$reponse = mysql_query("SELECT * FROM minichat ORDER BY ID DESC LIMIT 0,10");
-// On se déconnecte de MySQL
-mysql_close();
-// Puis on fait une boucle pour afficher tous les résultats :
-while($donnees = mysql_fetch_array($reponse))
-{
-?>
-
-<p><strong><?php echo$donnees['pseudo']; ?></strong> : <?php echo $donnees['message']; ?></p>
-
-<?php
-}
-// Fin de la boucle, le script est terminé !
 ?>
 
 </body>
--- minichat_post.php
+++ minichat_post.php
@@ -1,19 +1,23 @@
+
+
 <?php
-
-// Connexion à la base de données
-try
+// Maintenant on doit récupérer les 10 dernières entrées de la table
+// On se connecte d'abord à MySQL :
+mysql_connect("localhost", "root", "");
+mysql_select_db("minichat");
+// On utilise la requête suivante pour récupérer les 10 derniers messages :
+$reponse = mysql_query("SELECT * FROM minichat ORDER BY ID DESC LIMIT 0,10");
+// On se déconnecte de MySQL
+mysql_close();
+// Puis on fait une boucle pour afficher tous les résultats :
+while($donnees = mysql_fetch_array($reponse))
 {
-	$bdd = new PDO('mysql:host=localhost;dbname=minichat;charset=utf8', 'root', '');
-}
-catch(Exception $e)
-{
-        die('Erreur : '.$e->getMessage());
-}
+?>
 
-// Insertion du message à l'aide d'une requête préparée
-$req = $bdd->prepare('INSERT INTO minichat (pseudo, message) VALUES(?, ?)');
-$req->execute(array($_POST['pseudo'], $_POST['message']));
+<p><strong><?php echo$donnees['pseudo']; ?></strong> : <?php echo $donnees['message']; ?></p>
 
-// Redirection du visiteur vers la page du minichat
+<?php
+}
+// Fin de la boucle, le script est terminé !
 header('Location: minichat.php');
 ?>
