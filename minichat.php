24ec28431cbc91967f27c51dae20d7f3bf77a153
metabis <metabis.n83gomes@gmail.com>
2019-06-27 18:03:01 +0200

    test chat

--- minichat.php
+++ minichat.php
@@ -25,7 +25,7 @@
 // Connexion à la base de données
 try
 {
-	$bdd = new PDO('mysql:host=localhost;dbname=mydbtest;charset=utf8', 'root', '');
+	$bdd = new PDO('mysql:host=localhost;dbname=minichat;charset=utf8', 'root', '');
 }
 catch(Exception $e)
 {
