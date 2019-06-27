58db3235a1f3b8b76ca7ade05d2b22dbfa7f95a9
metabis <metabis.n83gomes@gmail.com>
2019-06-27 17:53:48 +0200

    modif minichat

--- minichat2.php
+++ minichat2.php
@@ -36,26 +36,5 @@ Votre message : <input type="text" name="message" /><br />
 <input type="submit" value="Envoyer" />
 </form>
 
-<?php
-// Maintenant on doit récupérer les 10 dernières entrées de la table
-// On se connecte d'abord à MySQL :
-mysql_connect("mysql:host=localhost", "root", "");
-mysql_select_db("mydbtest");
-// On utilise la requête suivante pour récupérer les 10 derniers messages :
-$reponse = mysql_query("SELECT * FROM minichat ORDER BY ID DESC LIMIT 0,10");
-// On se déconnecte de MySQL
-mysql_close();
-// Puis on fait une boucle pour afficher tous les résultats :
-while($donnees = mysql_fetch_array($reponse))
-{
-?>
-
-<p><strong><?php echo$donnees['pseudo']; ?></strong> : <?php echo $donnees['message']; ?></p>
-
-<?php
-}
-// Fin de la boucle, le script est terminé !
-?>
-
 </body>
 </html>
