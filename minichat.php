c2bf7ac3cfbcf05e11765e7305db682f2f085cfa
metabis <metabis.n83gomes@gmail.com>
2019-06-28 14:52:53 +0200

    test minichat

--- minichat.php
+++ minichat.php
@@ -1,25 +1,51 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Mini-chat</title>
-        <link rel="stylesheet" href="design.css" type="text/css" title="Design" />
-    </head>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd>
 
-    <body>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+
+<head>
+  <title>Site</title>
+  <link rel="stylesheet" href="design.css" type="text/css" title="Design" />
+</head>
+<body>
 <div id="global">
-  <table border = 0 >
-    <tr>
-  <td colspan=0><div="entete" ></td>
-    </tr>
-   <tr>
-   <td height=20px> &nbsp;  </td>
+<table border = 0 >
+  <tr>
+<td><img src="banpub.png" div="entete" ></td>
+  </tr>
+
+ <tr>
    <td >&nbsp;</td>
+<tr><td>
+   <?php include('menu.php');
+   ?></td></tr><td >&nbsp;</td>
+   <?php
 
-   </tr>
+   try
+   {
+   	$bdd = new PDO('mysql:host=localhost;dbname=minichat;charset=utf8', 'root', '');
+   }
+   catch(Exception $e)
+   {
+           die('Erreur : '.$e->getMessage());
+   }
+
+
+   $reponse = $bdd->query('SELECT pseudo, message FROM minichat ORDER BY ID DESC LIMIT 0, 10');
+
+
+   while ($donnees = $reponse->fetch())
+   {
+   	echo '<p><strong>' . htmlspecialchars($donnees['pseudo']) . '</strong> : ' . htmlspecialchars($donnees['message']) . '</p>';
+   }
+
+   $reponse->closeCursor();
+
+   ?>
+
    <tr>
 
-   <td><div id="corps">
+ <td><div id="corps">
     <form action="minichat_post.php" method="post">
         <p>
         <label for="pseudo">Pseudo</label> : <input type="text" name="pseudo" id="pseudo" /><br />
@@ -28,42 +54,13 @@
         <input type="submit" value="Envoyer" />
 	</p>
     </form>
-  </div></td>
-   <td >&nbsp;</td>
-  <td valign=top>
-  </div></td>
-   </tr>
-   <tr>
-   <td heigh=20px> &nbsp;  </td>
-   <td >&nbsp;</td>
-
-   </tr>
-   <tr>
-   <td colspan=3>
-<?php
-
-try
-{
-	$bdd = new PDO('mysql:host=localhost;dbname=minichat;charset=utf8', 'root', '');
-}
-catch(Exception $e)
-{
-        die('Erreur : '.$e->getMessage());
-}
+</td></tr>
+	</div>
 
 
-$reponse = $bdd->query('SELECT pseudo, message FROM minichat ORDER BY ID DESC LIMIT 0, 10');
-
-
-while ($donnees = $reponse->fetch())
-{
-	echo '<p><strong>' . htmlspecialchars($donnees['pseudo']) . '</strong> : ' . htmlspecialchars($donnees['message']) . '</p>';
-}
-
-$reponse->closeCursor();
-
-?>
-</td>
+ <tr>
+ <td> <?php include('pied_de_page.php');
+?></td>
  </tr>
  </table>
  </div>
