049fc16dca849dec46cac98e686894b0496aabe1
metabis <metabis.n83gomes@gmail.com>
2019-06-28 09:22:06 +0200

    test

--- minichat.php
+++ minichat.php
@@ -41,7 +41,7 @@
    <tr>
    <td colspan=3>
 <?php
-// Connexion à la base de données
+
 try
 {
 	$bdd = new PDO('mysql:host=localhost;dbname=minichat;charset=utf8', 'root', '');
@@ -51,10 +51,10 @@ catch(Exception $e)
         die('Erreur : '.$e->getMessage());
 }
 
-// Récupération des 10 derniers messages
+
 $reponse = $bdd->query('SELECT pseudo, message FROM minichat ORDER BY ID DESC LIMIT 0, 10');
 
-// Affichage de chaque message (toutes les données sont protégées par htmlspecialchars)
+
 while ($donnees = $reponse->fetch())
 {
 	echo '<p><strong>' . htmlspecialchars($donnees['pseudo']) . '</strong> : ' . htmlspecialchars($donnees['message']) . '</p>';
--- minichat_post.php
+++ minichat_post.php
@@ -1,5 +1,5 @@
 <?php
-// Connexion à la base de données
+
 try
 {
 	$bdd = new PDO('mysql:host=localhost;dbname=minichat;charset=utf8', 'root', '');
@@ -9,10 +9,10 @@ catch(Exception $e)
         die('Erreur : '.$e->getMessage());
 }
 
-// Insertion du message à l'aide d'une requête préparée
+
 $req = $bdd->prepare('INSERT INTO minichat (pseudo, message) VALUES(?, ?)');
 $req->execute(array($_POST['pseudo'], $_POST['message']));
 
-// Redirection du visiteur vers la page du minichat
+
 header('Location: minichat.php');
 ?>
